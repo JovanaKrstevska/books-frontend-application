5efd3c4f8490d6a7b5f0c276af56e529b35a5497
JovanaKrstevska <100038564+JovanaKrstevska@users.noreply.github.com>
2022-04-25 14:37:58 +0200

    Initial commit

---
+++ src/components/Books/BookTerm/bookTerm.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+
+
+const bookTerm = (props) => {
+    return (
+        <tr>
+            <td scope={"col"}>{props.term.name}</td>
+            <td scope={"col"}>{props.term.category.name}</td>
+            <td scope={"col"}>{props.term.author.name} {props.term.author.surname}</td>
+            <td scope={"col"}>{props.term.availableCopies}</td>
+            <td scope={"col"} className={"text-right"}>
+                <Link className={"btn btn-info ml-2"}
+                      onClick={() => props.onMark(props.term.id)}
+                      to={`/books`}>
+                    Mark as taken
+                </Link>
+                <Link className={"btn btn-warning ml-2"}
+                      onClick={() => props.onEdit(props.term.id)}
+                      to={`/books/edit/${props.term.id}`}>
+                    Edit
+                </Link>
+                <a title={"Delete"} className={"btn btn-danger ml-2"}
+                   onClick={() => props.onDelete(props.term.id)}>
+                    Delete
+                </a>
+            </td>
+
+        </tr>
+
+    )
+}
+export default bookTerm;
